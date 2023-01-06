99e43321f4fa357beb8965559bd9cb0638d8e856
Lucas williams <lucaswilliamscesar@gmail.com>
2023-01-06 06:55:31 -0300

    product, category, order, orderitem

--- backend/src/main/java/com/devlucas/commerce/entities/User.java
+++ backend/src/main/java/com/devlucas/commerce/entities/User.java
@@ -4,12 +4,14 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
 @Entity
 @Table(name = "tb_user")
@@ -19,6 +21,8 @@ public class User {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
+	
+	@Column(unique = true)
 	private String email;
 	private String phone;
 	private LocalDate birthDate;
