2276b778fe873217d35c522a1e7d62fb359668a4
Lucas williams <lucaswilliamscesar@gmail.com>
2023-01-09 06:37:59 -0300

    Get products paged

--- backend/src/main/java/com/devlucas/commerce/controllers/ProductController.java
+++ backend/src/main/java/com/devlucas/commerce/controllers/ProductController.java
@@ -1,16 +1,14 @@
 package com.devlucas.commerce.controllers;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.devlucas.commerce.dto.ProductDto;
-import com.devlucas.commerce.entities.Product;
-import com.devlucas.commerce.repositories.ProductRepository;
 import com.devlucas.commerce.service.ProductService;
 
 @RestController
@@ -23,5 +21,10 @@ public class ProductController {
 	@GetMapping(value = "/{id}")
 	public ProductDto findById(@PathVariable Long id) {
 		return service.findById(id);
+	}
+	
+	@GetMapping
+	public Page<ProductDto> findAll(Pageable pageable) {
+		return service.findAll(pageable);
 	}
 }
