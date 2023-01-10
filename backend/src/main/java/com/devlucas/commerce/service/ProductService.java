eebe71857e5eee87caf5bf4cea4e830d9d9e1694
Lucas williams <lucaswilliamscesar@gmail.com>
2023-01-10 05:31:36 -0300

    DELETE product

--- backend/src/main/java/com/devlucas/commerce/service/ProductService.java
+++ backend/src/main/java/com/devlucas/commerce/service/ProductService.java
@@ -48,6 +48,11 @@ public class ProductService {
 		entity = repository.save(entity);
 		return new ProductDto(entity);
 	}
+	
+	@Transactional
+	public void delete(Long id) {
+		repository.deleteById(id);
+	}
 
 	private void copyDtoToEntity(ProductDto dto, Product entity) {
 		entity.setName(dto.getName());
